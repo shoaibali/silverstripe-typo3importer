55d034066f99ca4e71da2f23469dcc3d9b905df9
Shoaib Ali <shoaib@catalyst.net.nz>
2014-07-18 18:38:41 +1200

    Added ability to skip hidden content and images working also converted all tabs to spaces

--- code/Typo3Importer.php
+++ code/Typo3Importer.php
@@ -2,157 +2,160 @@
 <?php
 
 class Typo3Importer extends Page_Controller {
-	static $allowed_actions = array(
-		'Form',
-		'bulkimport',
-		'complete'
-	);
+  static $allowed_actions = array(
+    'Form',
+    'bulkimport',
+    'complete'
+  );
 
-	function __construct() {
-		$dataRecord = new Page();
-		$dataRecord->Title = $this->Title();
-		$dataRecord->URLSegment = get_class($this);
-		$dataRecord->ID = -1;
-		parent::__construct($dataRecord);
-	}
+  function __construct() {
+    $dataRecord = new Page();
+    $dataRecord->Title = $this->Title();
+    $dataRecord->URLSegment = get_class($this);
+    $dataRecord->ID = -1;
+    parent::__construct($dataRecord);
+  }
 
-	function init() {
-		parent::init();
-		if(!Permission::check('ADMIN')) Security::permissionFailure();
-	}
-	
-	function Title() {
-		return "Typo3 Importer";
-	}
-	
-	function Content() {
-		$msg = <<<HTML
-		<p>This tool will let you migrate site structure and contents from a typo3 uncompressed in to SilverStripe:</p>
+  function init() {
+    parent::init();
+    if(!Permission::check('ADMIN')) Security::permissionFailure();
+  }
+  
+  function Title() {
+    return "Typo3 Importer";
+  }
+  
+  function Content() {
+    $msg = <<<HTML
+    <p>This tool will let you migrate site structure and contents from a typo3 uncompressed in to SilverStripe:</p>
 HTML;
 
-		// check directory exists
-		if( $folder = Folder::find('typo3import') ){
-			$msg .= <<<HTML
-			<p>&#10004 Directory called typo3import found</p>
+    // check directory exists
+    if( $folder = Folder::find('typo3import') ){
+      $msg .= <<<HTML
+      <p>&#10004 Directory called typo3import found</p>
 HTML;
-			// then also check if it has xml files
-			if( Folder::find('typo3import')->hasChildren() ){
-				$files = $folder->Children();
-				$file_names = "";
+      // then also check if it has xml files
+      if( Folder::find('typo3import')->hasChildren() ){
+        $files = $folder->Children();
+        $file_names = "";
 
-				foreach($files as $file)
-					$file_names .= '<li>'. $file->getFilename() .'</li>';
+        foreach($files as $file)
+          $file_names .= '<li>'. $file->getFilename() .'</li>';
 
-				$msg .= <<<HTML
-				<p>&#10004 Typo3 exported XML files in typo3import directory found</p>
-				<ul>
-					$file_names
-				</ul>
+        $msg .= <<<HTML
+        <p>&#10004 Typo3 exported XML files in typo3import directory found</p>
+        <ul>
+          $file_names
+        </ul>
 HTML;
-			} else {
-				$msg .= <<<HTML
-				<p>&times; No Typo3 XML files found in typo3import directory</p>
+      } else {
+        $msg .= <<<HTML
+        <p>&times; No Typo3 XML files found in typo3import directory</p>
 HTML;
-			}
+      }
 
-		} else {
-			Folder::find_or_make('typo3import');
-			$msg .= <<<HTML
-			<p>A directory called typo3import in assets has been created for you.
-			Please upload all typo3 exported XML to it</p>
+    } else {
+      Folder::find_or_make('typo3import');
+      $msg .= <<<HTML
+      <p>A directory called typo3import in assets has been created for you.
+      Please upload all typo3 exported XML to it</p>
 HTML;
-		}
-
-		 return $msg;
-	}
-	
-	function Form() {
-		$deleteExistingCheckBox = new CheckboxField("DeleteExisting", "Clear out all existing Typo3 Imported content?");
-		$deleteExistingCheckBox->setValue(TRUE);
-
-		$publishAllCheckBox = new CheckboxField("PublishAll", "Publish everything after the import?");
-
-
-		// No need to publish if some are already.
-		if(Versioned::get_by_stage('Typo3Page', 'Live')->Count() > 0)
-			$publishAllCheckBox->setValue(TRUE);
-		
-
-		return new Form($this, "Form", new FieldList(
-			$deleteExistingCheckBox,
-			$publishAllCheckBox
-		), new FieldList(
-			new FormAction("bulkimport", "Being Migration")
-		));
-	}
-	
-	function bulkimport($data, $form) {
-
-		Versioned::reading_stage('Stage');
-
-    if(isset($data['DeleteExisting']) && $data['DeleteExisting']) {
-     // get all the Typo3Pages
-     $Typo3Pages = Typo3Page::get();
-     foreach($Typo3Pages as $Typo3Page){
-       $Typo3Page->delete();
-     }
     }
 
-		
-		$files = array();
+     return $msg;
+  }
+  
+  function Form() {
+    $deleteExistingCheckBox = new CheckboxField("DeleteExisting", "Clear out all existing Typo3 Imported content?");
+    $deleteExistingCheckBox->setValue(TRUE);
 
-		if(!SapphireTest::is_running_test()) {
-			$folder = Folder::find('typo3import');
-			$children = $folder->Children();
-			foreach( $children as $child )
-				$files []= $child->getFullPath();
-		} else {
-				$files = $data["files"];
-		}
-
-		foreach($files as $file) {
-			$parentRefs = array();
-
-			$xml = simplexml_load_file($file);
+    $publishAllCheckBox = new CheckboxField("PublishAll", "Publish everything after the import?");
 
 
-			if(!SapphireTest::is_running_test())
-				echo "Procesing file => " . $file	. PHP_EOL . "<br/>";
+    // No need to publish if some are already.
+    if(Versioned::get_by_stage('Typo3Page', 'Live')->Count() > 0)
+      $publishAllCheckBox->setValue(TRUE);
+    
 
-			$root_tree = $xml->header->pagetree->node;
-			$site_tree = array();
+    return new Form($this, "Form", new FieldList(
+      $deleteExistingCheckBox,
+      $publishAllCheckBox
+    ), new FieldList(
+      new FormAction("bulkimport", "Being Migration")
+    ));
+  }
+  
+  function bulkimport($data, $form) {
 
-			$result = $this->buildTree($root_tree, $site_tree, $xml);
+    // force version to be Stage
+    Versioned::reading_stage('Stage');
 
-			$site_tree = array_pop($site_tree);
+    if(isset($data['DeleteExisting']) && $data['DeleteExisting']) {
 
-			$count = 1; // counter for number of elements
-			$level = 0; // counter for depth levels
-			$parentRefs = array(); // array to store parent id references
+      Versioned::reading_stage('Live');
+      // get all the Typo3Pages
+      $Typo3Pages = Typo3Page::get();
+      foreach($Typo3Pages as $Typo3Page){
+        $Typo3Page->delete();
+      }
 
-			$iterator = new RecursiveArrayIterator(array_filter($site_tree)); 
-			//iterator_apply($iterator, self::migrate($iterator, $level, $count, $parentRefs); 
-			self::migrate($iterator, $level, $count, $parentRefs); 
+    }
 
-			// cleanup memory
-			unset($iterator);
-			unset($site_tree);
-			unset($parentRefs);
-		}
-		
-		//Director::redirect($this->Link() . 'complete');		
-	}
-	
-	function complete() {
-		return array(
-			"Content" => "<p>Thanks! Your site tree & content has been imported.</p>",
-			"Form" => " ",
-		);
-	}
+    $files = array();
 
-	function buildTree($root, &$site_tree, $full_xml) {
-	  $sub_tree = array();
-	  $root_uid = (string) $root->uid;
+    if(!SapphireTest::is_running_test()) {
+      $folder = Folder::find('typo3import');
+      $children = $folder->Children();
+      foreach( $children as $child )
+        $files []= $child->getFullPath();
+    } else {
+        $files = $data["files"];
+    }
+
+    foreach($files as $file) {
+      $parentRefs = array();
+
+      $xml = simplexml_load_file($file);
+
+
+      if(!SapphireTest::is_running_test())
+        echo "Procesing file => " . $file . PHP_EOL . "<br/>";
+
+      $root_tree = $xml->header->pagetree->node;
+      $site_tree = array();
+
+      $result = $this->buildTree($root_tree, $site_tree, $xml);
+
+      $site_tree = array_pop($site_tree);
+
+      $count = 1; // counter for number of elements
+      $level = 0; // counter for depth levels
+      $parentRefs = array(); // array to store parent id references
+
+      $iterator = new RecursiveArrayIterator(array_filter($site_tree)); 
+      //iterator_apply($iterator, self::migrate($iterator, $level, $count, $parentRefs); 
+      self::migrate($iterator, $level, $count, $parentRefs); 
+
+      // cleanup memory
+      unset($iterator);
+      unset($site_tree);
+      unset($parentRefs);
+    }
+    
+    //Director::redirect($this->Link() . 'complete');   
+  }
+  
+  function complete() {
+    return array(
+      "Content" => "<p>Thanks! Your site tree & content has been imported.</p>",
+      "Form" => " ",
+    );
+  }
+
+  function buildTree($root, &$site_tree, $full_xml) {
+    $sub_tree = array();
+    $root_uid = (string) $root->uid;
 
     $sub_tree['uid'] = $root_uid;
     $sub_tree['title'] = self::getTitle($root, $full_xml);
@@ -161,12 +164,13 @@ HTML;
     $content_string = "";
 
     foreach($content_array as $c => $v) {
-    	$content_string .= "<h2>" . $v["header"] ."</h2>" . $v["bodytext"];
+      // silly idea to add <h2> tags
+      $content_string .= "<h2>" . $v["header"] ."</h2>" . $v["bodytext"];
     }
 
     $sub_tree['content'] = $content_string;
      // this (pid) must remain the very last thing that gets added to array
-		$sub_tree['pid'] = self::getPID($root, $full_xml);
+    $sub_tree['pid'] = self::getPID($root, $full_xml);
 
     // If there is children
     if (!empty($root->children()->node)) {
@@ -175,12 +179,12 @@ HTML;
         $sub_tree['children'][$child_uid] = $this->buildTree($child, $site_tree, $full_xml);
       }
 
-	  	$site_tree[$root_uid] = $sub_tree;
-	  }
-	  return $sub_tree;
-	}
+      $site_tree[$root_uid] = $sub_tree;
+    }
+    return $sub_tree;
+  }
 
-	private static function migrate($iterator, $level, &$count, &$parentRefs) { 
+  private static function migrate($iterator, $level, &$count, &$parentRefs) { 
 
 
     while ( $iterator->valid() ) { 
@@ -191,140 +195,164 @@ HTML;
 
             if( ($iterator->current() !== "") ){
 
-            	if( $iterator->key() == "uid" ) {
-            		$uID = $iterator->current();
-            	}
+              if( $iterator->key() == "uid" ) {
+                $uID = $iterator->current();
+              }
 
-            	if( $iterator->key() == "title" ) {
-            		$title = $iterator->current();
-            	}
+              if( $iterator->key() == "title" ) {
+                $title = $iterator->current();
+              }
 
-            	if( $iterator->key() == "description" ) {
-            		$description = $iterator->current();
-            	}
+              if( $iterator->key() == "description" ) {
+                $description = $iterator->current();
+              }
 
-            	if( $iterator->key() == "content" ){
-            		$bodytext = $iterator->current();
-            	}
+              if( $iterator->key() == "content" ){
+                $bodytext = $iterator->current();
+              }
 
-            	if( $iterator->key() == "pid" ) {
+              if( $iterator->key() == "pid" ) {
 
-            		$parentID = $iterator->current();
+                $parentID = $iterator->current();
 
-								$newPage = new Typo3Page();
-								$newPage->IsTypo3 = TRUE;
-								$newPage->Typo3UID = $uID;
-								$newPage->Typo3PID = $parentID;
-								$newPage->Title = $title;
-								$newPage->Content = ( isset($description) )? $description : "";
-            		$newPage->Content .= ( isset($bodytext) ) ? $bodytext : "";
+                $newPage = new Typo3Page();
+                $newPage->IsTypo3 = TRUE;
+                $newPage->Typo3UID = $uID;
+                $newPage->Typo3PID = $parentID;
+                $newPage->Title = $title;
 
-								$newPage->URLSegment = NULL;
+                
+                if(isset($bodytext)){
+                  //var_dump($bodytext);
+                }
 
-								// Set parent based on parentRefs;
-								if($level > 0) $newPage->ParentID = $parentRefs[$level-1];
+                $newPage->Content = ( isset($description) )? $description : "";
+                $newPage->Content .= ( isset($bodytext) ) ? $bodytext : "";
 
+                $newPage->URLSegment = NULL;
 
-								// echo "(" . $level . ")" . " <strong>ParentID</strong> => " . $parentID . 
-								// 			" <strong>UID</strong> => " . $uID .
-								// 			" <strong>TITLE</strong> => " . $title .  PHP_EOL . "<br/>";
-
-
-								// Don't write the page until we have Title, Content and ParentID
-								$newPage->write();
-
-								if(isset($data['PublishAll']) && $data['PublishAll']) $newPage->publish('Stage', 'Live');
-
-									// Populate parentRefs with the most recent page at every level.   Necessary to build tree
-								$parentRefs[$level] = $newPage->ID;
-
-								// Remove no-longer-relevant children from the parentRefs.  Allows more graceful acceptance of files
-								// with errors
-								for($i=sizeof($parentRefs)-1;$i>$level;$i--) unset($parentRefs[$i]);
-
-								if(!SapphireTest::is_running_test())
-									echo"<li>Written #$newPage->ID: $newPage->Title (child of $newPage->ParentID)</li>";
+                // Set parent based on parentRefs;
+                if($level > 0) $newPage->ParentID = $parentRefs[$level-1];
 
 
-								// Memory cleanup
-								$newPage->destroy();
-								unset($newPage);
-
-								$level++;  // counter for depth
+                // echo "(" . $level . ")" . " <strong>ParentID</strong> => " . $parentID . 
+                //      " <strong>UID</strong> => " . $uID .
+                //      " <strong>TITLE</strong> => " . $title .  PHP_EOL . "<br/>";
 
 
-            	}
+                // Don't write the page until we have Title, Content and ParentID
+                $newPage->write();
+
+                //if(isset($data['PublishAll']) && $data['PublishAll']) 
+                $newPage->publish('Stage', 'Live');
+
+                  // Populate parentRefs with the most recent page at every level.   Necessary to build tree
+                $parentRefs[$level] = $newPage->ID;
+
+                // Remove no-longer-relevant children from the parentRefs.  Allows more graceful acceptance of files
+                // with errors
+                for($i=sizeof($parentRefs)-1;$i>$level;$i--) unset($parentRefs[$i]);
+
+                if(!SapphireTest::is_running_test())
+                  echo"<li>Written #$newPage->ID: $newPage->Title (child of $newPage->ParentID)</li>";
+
+
+                // Memory cleanup
+                $newPage->destroy();
+                unset($newPage);
+
+                $level++;  // counter for depth
+
+
+              }
 
               $count++;  // counter for each element in array
             }
         } 
       $iterator->next();     
-		}
-	}
+    }
+  }
 
-	private static function getTitle($node, $xml){
-	  $title_xpath = "/T3RecordDocument//records/table/rec[@index='" . (string) $node->uid . "']/title";
-	  $title = "";
-	  $node_uid = (string)$node->uid;
+  private static function getTitle($node, $xml){
+    $title_xpath = "/T3RecordDocument//records/table/rec[@index='" . (string) $node->uid . "']/title";
+    $title = "";
+    $node_uid = (string)$node->uid;
 
-	  if(!empty($node_uid)){
-	   $title = $xml->xpath($title_xpath);
-	   $title = (string) $title[0];
-	  }
+    if(!empty($node_uid)){
+     $title = $xml->xpath($title_xpath);
+     $title = (string) $title[0];
+    }
 
-	  return $title;
-	}
+    return $title;
+  }
 
-	private static function getDescription($node, $xml){
-	  $description_xpath = "/T3RecordDocument/records/tablerow[@index='pages:" . (string) $node->uid . "']/fieldlist/field[@index='description']";
-	  $description = "";
-	  $node_uid = (string) $node->uid;
+  private static function getDescription($node, $xml){
+    $description_xpath = "/T3RecordDocument/records/tablerow[@index='pages:" . (string) $node->uid . "']/fieldlist/field[@index='description']";
+    $description = "";
+    $node_uid = (string) $node->uid;
+    // TODO Skip hidden content see getContent:$hidden
+    if(!empty($node_uid)){
+      $description = $xml->xpath($description_xpath); 
+      $description = (string) $description[0];
+    }
+    return $description;
+  }
 
-	  if(!empty($node_uid)){
-	    $description = $xml->xpath($description_xpath); 
-	    $description = (string) $description[0];
-	  }
-	  return $description;
-	}
+  private static function getContent($node, $xml){
+    $content_xpath = "/T3RecordDocument/header/pid_lookup/page_contents[@index='".(string) $node->uid . "']/table[@index='tt_content']/item";
+    $content = "";
+    $node_uid = (string) $node->uid;
+    $content_complete  = array(); // this array will hold all item id's for their respective contents
 
-	private static function getContent($node, $xml){
-		$content_xpath = "/T3RecordDocument/header/pid_lookup/page_contents[@index='".(string) $node->uid . "']/table[@index='tt_content']/item";
-		$content = "";
-		$node_uid = (string) $node->uid;
-		$content_complete	 = array(); // this array will hold all item id's for their respective contents
+    // TODO maybe do some content processing here i.e remove or reconstruct links and strip html tags etc.
+    if(!empty($node_uid)){
+      $content = $xml->xpath($content_xpath);
+      foreach($content as $ck){ 
 
-		// TODO maybe do some content processing here i.e remove or reconstruct links and strip html tags etc.
-		if(!empty($node_uid)){
-			$content = $xml->xpath($content_xpath);
-			foreach($content as $ck){ 
+        // we are going to skip all hidden content
+        $hidden =  $xml->xpath("/T3RecordDocument/records/tablerow[@index='tt_content:".(string)$ck["index"]."']/fieldlist/field[@index='hidden']");
 
-				$header = $xml->xpath("/T3RecordDocument/records/tablerow[@index='tt_content:".(string)$ck["index"]."']/fieldlist/field[@index='header']");
-				$header = (string) $header[0];
+        if( !(string)$hidden[0] ) {
+          $header = $xml->xpath("/T3RecordDocument/records/tablerow[@index='tt_content:".(string)$ck["index"]."']/fieldlist/field[@index='header']");
+          $header = (string) $header[0];
 
-				$bodytext = $xml->xpath("/T3RecordDocument/records/tablerow[@index='tt_content:".(string)$ck["index"]."']/fieldlist/field[@index='bodytext']");
-				$bodytext = html_entity_decode( (string) $bodytext[0], ENT_QUOTES, "UTF-8");
+          $bodytext = $xml->xpath("/T3RecordDocument/records/tablerow[@index='tt_content:".(string)$ck["index"]."']/fieldlist/field[@index='bodytext']");
 
-				$content_complete []= array("header" => $header,
-																		"bodytext" => $bodytext);
+          $bodytext = (string) $bodytext[0];
 
-			}
-		}
+          // re-link images to /assets directory
+          if( (strpos($bodytext, "img")) ){
+            if( (strpos($bodytext, "###CMS_URL###") !== FALSE) ) {          
+              $bodytext = str_replace("###CMS_URL###", "/assets/", $bodytext);
+            } else {
+              $bodytext = str_replace("fileadmin", "/assets/fileadmin/", $bodytext);
+            }
+          }
 
-		return $content_complete;
+          $bodytext = html_entity_decode( (string) $bodytext, ENT_QUOTES, "UTF-8");
 
-	}
+          $content_complete []= array("header" => $header,
+                                      "bodytext" => $bodytext);
+        }
+      }
 
-	private static function getPID($node, $xml){
-	  $pid_xpath = "/T3RecordDocument/header/records/table/rec[@index='". (string) $node->uid . "']/pid";
-	  $pid = "";
-	  $node_uid = (string)$node->uid;
+    }
 
-	  if(!empty($node_uid)){
-	   $pid = $xml->xpath($pid_xpath);
-	   $pid = (string)$pid[0];
-	  }
+    return $content_complete;
 
-	  return $pid;
-	}
-	
+  }
+
+  private static function getPID($node, $xml){
+    $pid_xpath = "/T3RecordDocument/header/records/table/rec[@index='". (string) $node->uid . "']/pid";
+    $pid = "";
+    $node_uid = (string)$node->uid;
+
+    if(!empty($node_uid)){
+     $pid = $xml->xpath($pid_xpath);
+     $pid = (string)$pid[0];
+    }
+
+    return $pid;
+  }
+  
 }
